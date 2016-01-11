8864d37cf57fb87b6a019679bc664d05a9c5d2a7
Jim Hester <james.f.hester@gmail.com>
2016-01-11 17:09:02 -0500

    Tweak tar commands

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/2f5ca88b22431a1ec36d092e728c943c62a00133/texlive.tar.xz | tar xJC ~
-curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/2f5ca88b22431a1ec36d092e728c943c62a00133/pandoc.tar.xz | tar xJC ~
+curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/2f5ca88b22431a1ec36d092e728c943c62a00133/texlive.tar.xz | tar xC ~
+curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/2f5ca88b22431a1ec36d092e728c943c62a00133/pandoc.tar.xz | tar xC ~
 
 wget -q 'https://github.com/gaborcsardi/r-builder/archive/travis-3.2.3.zip' && unzip travis-3.2.3.zip -d ~

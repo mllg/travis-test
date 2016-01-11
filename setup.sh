8b9fdf14b826e73ed775b48475b0bbf5ae592099
Jim Hester <james.f.hester@gmail.com>
2016-01-11 17:20:02 -0500

    Move to R-bin like r-builder

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/2f5ca88b22431
 curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/2f5ca88b22431a1ec36d092e728c943c62a00133/pandoc.tar.xz | tar xC ~
 
 wget -q 'https://github.com/gaborcsardi/r-builder/archive/travis-3.2.3.zip' && unzip travis-3.2.3.zip -d ~
+mv ~/r-builder-travis-3.2.3/R-3.2.3/ ~/R-bin

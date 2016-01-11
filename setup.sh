fc9e9a22ded80ef2b791a69c9018add3d9a538c7
Jim Hester <james.f.hester@gmail.com>
2016-01-11 16:38:57 -0500

    Tweaks

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/2f5ca88b22431
 
 wget 'https://github.com/gaborcsardi/r-builder/archive/travis-3.2.3.zip' && unzip travis-3.2.3.zip
 export PATH=$HOME/texlive/bin/x86_64-linux:$HOME/pandoc/:$HOME/r-builder-travis-3.2.3/R-3.2.3/bin/:$PATH
+echo $PATH

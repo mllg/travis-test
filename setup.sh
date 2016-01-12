731701215b4ec8c7dd3c663743f73aa799d5e9c7
Jim Hester <james.f.hester@gmail.com>
2016-01-12 14:19:07 -0500

    Compile curl

--- setup.sh
+++ setup.sh
@@ -3,9 +3,15 @@
 curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/45ee129b3b543424435efb0bdccf9af3cce40ba8/texlive.tar.xz | tar xJC ~
 curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/45ee129b3b543424435efb0bdccf9af3cce40ba8/pandoc.tar.xz | tar xJC ~
 
-curl -L 'http://curl.haxx.se/download/curl-7.46.0.tar.gz' | tar xC ~ && \
+
+#curl -L 'https://www.openssl.org/source/openssl-1.0.2e.tar.gz' | tar xz && \
+  #cd openssl-1.0.2e && \
+  #./Configure --prefix=$HOME/curl && \
+  #make && make install
+
+curl -L 'http://curl.haxx.se/download/curl-7.46.0.tar.gz' | tar xzC ~ && \
   cd curl-7.46.0 && \
-  ./configure --prefix $HOME && \
+  ./configure --prefix $HOME/curl && \
   make && make install
 
 #curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/a5bc3d2df06545dbcfe065effe2de2c990feb3e7/curl.tar.xz | tar xJC ~

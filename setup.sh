3acb8558c617bf19c76d43745c9a1b18c2482b47
Jim Hester <james.f.hester@gmail.com>
2016-01-12 16:24:11 -0500

    Just use the tar files directly

--- setup.sh
+++ setup.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 
-wget -q -O - https://github.com/yihui/crandalf/raw/master/inst/scripts/install-texlive | bash
-wget -q -O - https://github.com/yihui/crandalf/raw/master/inst/scripts/install-pandoc | bash
-
-#curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/45ee129b3b543424435efb0bdccf9af3cce40ba8/texlive.tar.xz | tar xJC ~
-#curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/45ee129b3b543424435efb0bdccf9af3cce40ba8/pandoc.tar.xz | tar xJC ~
-
-
-##curl -L 'https://www.openssl.org/source/openssl-1.0.2e.tar.gz' | tar xz && \
-  ##cd openssl-1.0.2e && \
-  ##./Configure --prefix=$HOME/curl && \
-  ##make && make install
-
-##curl -L 'http://curl.haxx.se/download/curl-7.46.0.tar.gz' | tar xzC ~ && \
-  ##pushd ~/curl-7.46.0 && \
-  ##./configure --prefix $HOME && \
-  ##make && make install && \
-  ##popd
-
-#curl -L https://gist.github.com/jimhester/bd8740973cc17b7b9713/raw/fd70d47f54ef1df4978aae255e4e24cfb831cda3/curl.tar.xz | tar xJC ~
-#curl --version
+curl -L https://github.com/yihui/ubuntu-bin/releases/download/latest/texlive.tar.gz | tar xzC ~
+curl -L https://github.com/yihui/ubuntu-bin/releases/download/latest/pandoc.tar.gz | tar xJC ~
 
 curl -L -O 'https://github.com/gaborcsardi/r-builder/archive/travis-3.2.3.zip' && unzip -d ~ travis-3.2.3.zip && ln -s ~/r-builder-travis-3.2.3/R-3.2.3 ~/R-bin/

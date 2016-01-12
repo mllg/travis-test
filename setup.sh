52ca805f7e0bcb3dbd235f50ed52df94a4893385
Jim Hester <james.f.hester@gmail.com>
2016-01-12 16:56:15 -0500

    Comment out curl as it is now cached

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ curl -L https://github.com/yihui/ubuntu-bin/releases/download/latest/texlive.tar
 curl -L https://github.com/yihui/ubuntu-bin/releases/download/latest/pandoc.tar.gz | tar xzC ~
 
 
-curl -L 'http://curl.haxx.se/download/curl-7.46.0.tar.gz' | tar xzC ~ && \
-  pushd ~/curl-7.46.0 && \
-  ./configure --prefix $HOME && \
-  make && make install && \
-  popd
+#curl -L 'http://curl.haxx.se/download/curl-7.46.0.tar.gz' | tar xzC ~ && \
+  #pushd ~/curl-7.46.0 && \
+  #./configure --prefix $HOME && \
+  #make && make install && \
+  #popd
 
 curl -L -O 'https://github.com/gaborcsardi/r-builder/archive/travis-3.2.3.zip' && unzip -d ~ travis-3.2.3.zip && ln -s ~/r-builder-travis-3.2.3/R-3.2.3 ~/R-bin/

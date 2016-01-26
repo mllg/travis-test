9d117048bf00f220910a6341523a1e69411a3ac8
Jim Hester <james.f.hester@gmail.com>
2016-01-26 11:36:01 -0500

    Try r-devel

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 #curl -L https://github.com/yihui/ubuntu-bin/releases/download/latest/texlive.tar.gz | tar xzC ~
 #curl -L https://github.com/yihui/ubuntu-bin/releases/download/latest/pandoc.tar.gz | tar xzC ~
 
-curl -L -O 'https://github.com/gaborcsardi/r-builder/archive/travis-3.2.3.zip' && unzip -d ~ travis-3.2.3.zip && ln -s ~/r-builder-travis-3.2.3/R-3.2.3 ~/R-bin/
+curl -L 'https://s3.amazonaws.com/rstudio-travis/R-devel.xz' tar xzC ~

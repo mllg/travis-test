85901814278fd9b2e7364ecd406f65859d5133ee
Jim Hester <james.f.hester@gmail.com>
2016-01-28 10:34:09 -0500

    Fix tar command

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 #curl -L https://github.com/yihui/ubuntu-bin/releases/download/latest/texlive.tar.gz | tar xzC ~
 #curl -L https://github.com/yihui/ubuntu-bin/releases/download/latest/pandoc.tar.gz | tar xzC ~
 
-curl -L 'https://s3.amazonaws.com/rstudio-travis/R-devel.xz' tar xzC ~
+curl -L 'https://s3.amazonaws.com/rstudio-travis/R-devel.xz' | tar xzC ~
